a77915321655186f8b541fce5c71bb53799bc6d7
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-09-19 15:46:31 +0400

    added ajax moved the address selector

--- register.php
+++ register.php
@@ -191,6 +191,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     </style>
 </head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <body>
     <div class="form-container">
         <div class="form-column">
@@ -219,6 +220,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </div>
     </div>
 
-    <script src="ph-json/ph-address-selector.js"></script>
+    <script src="ph-address-selector.js"></script>
 </body>
 </html>
