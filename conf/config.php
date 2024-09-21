0775bc170a52e2d1b538aa6be051109bc7de31cb
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-09-21 10:37:05 +0400

    echo config

--- conf/config.php
+++ conf/config.php
@@ -12,4 +12,5 @@ $conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+// echo "Connected successfully";
 ?>
