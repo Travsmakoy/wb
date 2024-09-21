0cffa6aa54607f5a20983dc9c8d039af3ebd0383
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-09-21 10:33:26 +0400

    RUNNING ON ONLINE DATABASE

--- conf/config.php
+++ conf/config.php
@@ -1,14 +1,14 @@
 <?php
-// Database connection settings
-$host = 'localhost'; // or your database host
-$db = 'vapeshop1'; // the name of your database
-$user = 'root'; // your MySQL username
-$pass = 'travsmakoy'; // your MySQL password
+$servername = "26b.h.filess.io";
+$username = "inno_samehimdig";
+$password = "99c3181bd567e85e27f530066403f8443f41a2ca";
+$dbname = "inno_samehimdig";
+$port = 3307; // Add the port here
 
-// Create a new mysqli instance and connect to the database
-$conn = new mysqli($host, $user, $pass, $db);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// Check the connection
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
