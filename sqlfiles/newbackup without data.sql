46166a725e1d17963bbd0102e113fc90476925e6
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-10-14 17:26:01 +0400

    faqs

--- sqlfiles/newbackup without data.sql
+++ sqlfiles/newbackup without data.sql
@@ -45,6 +45,8 @@ CREATE TABLE `users` (
   CONSTRAINT `PRIMARY` PRIMARY KEY (`id`),
   CONSTRAINT `email` UNIQUE (`email`)
 );
+
+
 ALTER TABLE `products` ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE `products` ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`brand_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
@@ -62,3 +64,21 @@ INSERT INTO `categories` (`category_id`, `category_name`) VALUES
 (2, 'Vapes'),
 (3, 'Disposables'),
 (4, 'NEW CATEGORY');
+
+CREATE TABLE `faqs` (
+  `faq_id` int(11) NOT NULL,
+  `question` mediumtext NOT NULL,
+  `answer` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `faqs`
+--
+
+INSERT INTO `faqs` (`faq_id`, `question`, `answer`) VALUES
+(1, 'What are the different types of vaping devices available?', 'Vaping devices come in various types such as cigalikes, vape pens, pod mods, box mods, and mechanical mods.'),
+(2, 'Is vaping safer than smoking traditional cigarettes?', 'While vaping is considered to be less harmful than smoking traditional cigarettes, it is not completely risk-free. Vaping still involves inhaling aerosols containing nicotine and other chemicals.'),
+(3, 'How do I choose the right nicotine strength for my e-liquid?', 'Nicotine strength selection depends on factors like your smoking history and vaping habits. Beginners typically start with lower nicotine strengths (3-6mg/ml), while heavy smokers may opt for higher concentrations (12-18mg/ml).'),
+(4, 'What are the main ingredients in e-liquids?', 'E-liquids typically contain propylene glycol (PG), vegetable glycerin (VG), nicotine, and flavorings. Some may also contain distilled water or other additives.'),
+(5, 'How often should I change the coil in my vaping device?', 'Coil lifespan varies depending on usage, e-liquid composition, and maintenance. On average, coils should be replaced every 1-4 weeks to maintain optimal performance and flavor.'),
+(6, 'Are there any regulations regarding vaping products?', 'Regulations for vaping products vary by region. It\'s important to stay informed about local laws regarding sales, advertising, and usage of vaping devices and e-liquids.');
