c9866d1be92fdc02f81ad53041daf7bab2b19d7d
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-10-29 13:30:24 +0400

    test

--- login1.php
+++ login1.php
@@ -7,7 +7,6 @@
     <title>Responsive Registration Form</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
     <style>
         body {
             background-color: #0b062c;
